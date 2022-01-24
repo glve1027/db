857635e2aff8e1a89ea506cb88027eeab58f9ed3
johnny.gong <johnny.gong@shopee.com>
2022-01-24 17:25:46 +0800

    update

--- db/Classes/db.swift
+++ db/Classes/db.swift
@@ -7,13 +7,22 @@
 
 import Foundation
 
-class SaveManager {}
-class DeleteManager {}
-class FetchManager {}
+public protocol DataBaseProtocol {
+    func handle(data: [String: AnyObject]?)
+}
+public class SaveManager: DataBaseProtocol {
+    public func handle(data: [String: AnyObject]?) {}
+}
+public class DeleteManager {
+    public func handle(data: [String: AnyObject]?) {}
+}
+public class FetchManager {
+    public func handle(data: [String: AnyObject]?) {}
+}
 
-class DBManager {
-    static let shared = DBManager()
-    let save: SaveManager = SaveManager()
-    let delete: DeleteManager = DeleteManager()
-    let fetch: FetchManager = FetchManager()
+public class DBManager {
+    public static let shared = DBManager()
+    public let save: SaveManager = SaveManager()
+    public let delete: DeleteManager = DeleteManager()
+    public let fetch: FetchManager = FetchManager()
 }
